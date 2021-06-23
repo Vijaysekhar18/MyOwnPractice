e322b04453e60cab1b0c2c6ff95d4d7232412c79
Vijay Sekhar <vijay.sekhar18@gmail.com>
2021-06-23 20:36:52 +0530

    Updated the Test Line

--- Preparation/src/brillioAndWillsen/ArrayListSizeUnderstanding.java
+++ Preparation/src/brillioAndWillsen/ArrayListSizeUnderstanding.java
@@ -7,7 +7,7 @@ public class ArrayListSizeUnderstanding {
 	public static void main(String[] args) {
 		ArrayList<Integer> list = new ArrayList<>();
 		System.out.println(list.size());
-		
+		System.out.println("Test");
 		list.add(2);
 		list.add(3);
 		list.add(4);
